3c912b0cbb4251f3023c435e5a3ce4a39a04d0cc
Petro Podrezo <petro@podrezo.com>
2018-04-18 23:16:01 -0400

    Return JSON

--- server/parser.rb
+++ server/parser.rb
@@ -3,54 +3,105 @@ require 'date'
 require 'nokogiri'
 require 'open-uri'
 
+TIME_RANGE_REGEXP =
+    /(\d{1,2})(:(\d{1,2}))?(am|pm)? - (\d{1,2})(:(\d{1,2}))?(am|pm)?/
+# The date-time format that's going to be supplied at the end
+# e.g. 'Apr 24 5:36pm', year is assumed to be current one
+STRPTIME_FORMAT_DATETIME = '%b %d %l:%M%P'.freeze
+# ASSUMED_TIMEZONE = 'America/New_York'.freeze
+DEFAULT_DATETIME_OUTPUT_FORMAT = '%c %Z'.freeze
+
+class Activity
+  attr_accessor :name
+  attr_accessor :datetime_from
+  attr_accessor :datetime_to
+  
+  def initialize(activity_title, datetime_from, datetime_to)
+    @name = activity_title
+    @datetime_from = datetime_from
+    @datetime_to = datetime_to
+  end
+
+  def to_json_formattable
+    { 'name' => @name, 'from' => @datetime_from, 'to' => @datetime_to }
+  end
+end
+
+class Location
+  attr_accessor :name
+  attr_accessor :activities
+
+  def initialize(name)
+    self.name = name
+    self.activities = []
+  end
+
+  def to_json_formattable
+    { 'name' => @name, 'activities' => @activities.map(&:to_json_formattable) }
+  end
+end
+
+class Schedule
+  attr_accessor :locations
+
+  def initialize
+    self.locations = []
+  end
+
+  def to_json_formattable
+    { 'locations' => @locations.map(&:to_json_formattable) }
+  end
+end
+
 # Parses a given URL into meaningful data chunks
 class InfoParser
-  TIME_RANGE_REGEXP =
-    /(\d{1,2})(:(\d{1,2}))?(am|pm)? - (\d{1,2})(:(\d{1,2}))?(am|pm)?/
-  # The date-time format that's going to be supplied at the end
-  # e.g. 'Apr 24 5:36pm', year is assumed to be current one
-  STRPTIME_FORMAT_DATETIME = '%b %d %l:%M%P'.freeze
-  # ASSUMED_TIMEZONE = 'America/New_York'.freeze
-  DEFAULT_DATETIME_OUTPUT_FORMAT = '%c %Z'.freeze
-
   def self.parse_page_url(url)
-    # page = Nokogiri::HTML(open("leisure-drop-in.html"))
     page = Nokogiri::HTML(open(url))
-    self.parse_document(page)
+    parse_document(page)
   end
 
   def self.parse_document(page)
+    schedule = Schedule.new
     listings = page.css('.pfrProgramDescrList .pfrListing')
     listings.each { |location_html|
-    location_name = location_html.css('h2 a').text
-    # Sanity check - does the table have the expected columns?
-    table_header = location_html.css('table thead tr th').text.strip
-    raise "Unexpected table heading '#{table_header}'" if table_header != 'Program  Sun  Mon  Tue  Wed  Thu  Fri  Sat'
+      # Sanity check - does the table have the expected columns?
+      table_header = location_html.css('table thead tr th').text.strip
+      raise "Unexpected table heading '#{table_header}'" if table_header != 'Program  Sun  Mon  Tue  Wed  Thu  Fri  Sat'
+
+      # Create location
+      location_name = location_html.css('h2 a').text
+      location = Location.new(location_name)
+      schedule.locations.push(location)
+      parse_weeks(location, location_html)
+    }
+    schedule
+  end
+
+  def self.parse_weeks(location, location_html)
     weeks_html = location_html.css('table tbody tr')
-    weeks_html.each { |week_html|
+    weeks_html.each do |week_html|
       activity_title = week_html.css('th div').text.strip
-      row_title =  week_html.css('th').text.strip
+      row_title = week_html.css('th').text.strip
       week_dates =
         row_title[activity_title.length..row_title.length].strip.split(' to ')
       hours_cells_html = week_html.css('td')
-      hours_cells_html.each_with_index  { |hours_cell_html, day_offset|
-        hours_text = hours_cell_html.text.strip
-        # sometimes we get a blank with just a space
-        # TODO: it should not come back as blank!
-        next if hours_text.length == 1
+      parse_hours(location, hours_cells_html, week_dates[0], activity_title)
+    end
+  end
 
-        range = parse_range(week_dates[0], day_offset, hours_text)
-
-        # Assert that we have the matching day of the week
-        day_of_the_week_attr = hours_cell_html.attr('data-info')
-        raise "Unexpected day of the week '#{range[0].strftime('%a')}' doesn't match '#{day_of_the_week_attr}'" if range[0].strftime('%a') != day_of_the_week_attr
-
-        # location_name: York Recreation Centre
-        # activity_title: Leisure Swim
-        # logger.info("'#{activity_title}' at '#{location_name}'; '#{range[0].strftime(DEFAULT_DATETIME_OUTPUT_FORMAT)}' - '#{range[1].strftime(DEFAULT_DATETIME_OUTPUT_FORMAT)}'")
-      }
-    }
-}
+  def self.parse_hours(location, hours_html, week_start, activity_title)
+    hours_html.each_with_index do |hours_cell_html, day_offset|
+      hours_text = hours_cell_html.text.strip
+      # sometimes we get a blank with just a space
+      # TODO: it should not come back as blank!
+      next if hours_text.length == 1
+      range = parse_range(week_start, day_offset, hours_text)
+      # Assert that we have the matching day of the week
+      day_of_the_week_attr = hours_cell_html.attr('data-info')
+      raise "Unexpected day of the week '#{range[0].strftime('%a')}' doesn't match '#{day_of_the_week_attr}'" if range[0].strftime('%a') != day_of_the_week_attr
+      activity = Activity.new(activity_title, range[0], range[1])
+      location.activities.push(activity)
+    end
   end
 
   # Take a time range string and convert it to an array of two normalized times
@@ -78,8 +129,12 @@ class InfoParser
     times = normalize_times(times)
     # Create a string to be parsed
     # TODO: Figure out timezones
-    from_datetime = DateTime.strptime("#{date} #{times[0]}", STRPTIME_FORMAT_DATETIME)
-    to_datetime = DateTime.strptime("#{date} #{times[1]}", STRPTIME_FORMAT_DATETIME)
+    from_datetime = DateTime.strptime(
+      "#{date} #{times[0]}", STRPTIME_FORMAT_DATETIME
+    )
+    to_datetime = DateTime.strptime(
+      "#{date} #{times[1]}", STRPTIME_FORMAT_DATETIME
+    )
     from_datetime = from_datetime.next_day(offset_days)
     to_datetime = to_datetime.next_day(offset_days)
     [from_datetime, to_datetime]
--- server/server.rb
+++ server/server.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'logger'
+require 'json'
+
+require_relative 'parser'
+
+# Guide on how to create server at
+# https://x-team.com/blog/how-to-create-a-ruby-api-with-sinatra/
+# Style guide at https://github.com/bbatsov/ruby-style-guide
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
 
-logger.info("Server started")
+logger.info('Server started')
 
-get '/' do
-  'Welcome to BookList!'
+before do
+  content_type 'application/json'
+end
+
+get '/api/schedule' do
+  InfoParser.parse_page_url('leisure-drop-in.html').to_json_formattable.to_json
 end
