2ab0397345bcbd9b313e1836fd3aa49992dd81ab
Petro Podrezo <petro@podrezo.com>
2018-06-21 09:59:06 -0400

    Add script loader for firebase import from node_modules

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+import 'script-loader!firebase/firebase-app';
+import 'script-loader!firebase/firebase-firestore';
+
+// These are public facing keys required by Firebase to be available to the
+// clients. It is not a security risk.
+var config = {
+  apiKey: "AIzaSyDJMLTmpU6Z-Pu8eXG6iK2c8APkoH_NIlA",
+  authDomain: "poolfinder-2679b.firebaseapp.com",
+  databaseURL: "https://poolfinder-2679b.firebaseio.com",
+  projectId: "poolfinder-2679b",
+  storageBucket: "poolfinder-2679b.appspot.com",
+  messagingSenderId: "1093689089159"
+};
+firebase.initializeApp(config);
+const firestore = firebase.firestore();
+const settings = { /* your settings... */ timestampsInSnapshots: true };
+firestore.settings(settings);
+
+export default firebase;
