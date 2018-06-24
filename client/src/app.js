2953eb86e179231690be573469fde6a4b59cf282
Petro Podrezo <petro@podrezo.com>
2018-06-24 09:17:43 -0400

    Load pool times

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,9 @@
 import './assets/scss/app.scss';
-// import firebase from './js/firebase';
+import firebase from './js/firebase';
 import './js/react-root';
 import './js/sidemenu';
 
-// var db = firebase.firestore();
+var db = firebase.firestore();
 
 // db
 //   .collection("locations")
@@ -15,3 +15,20 @@ import './js/sidemenu';
 //     });
 //   });
 
+var now = new Date();
+var later = new Date(Date.now() + 1000*60*60*5);
+
+db
+  .collection("schedule")
+  .where("from", ">", now)
+  .where("from", "<", later)
+  .limit(5)
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      var d = doc.data();
+      var from = new Date(d.from.seconds*1000);
+      var to = new Date(d.to.seconds*1000);
+      console.log(`${d.activity_name} @ ${d.location_name}, ${from} --> ${to}`);
+    });
+  });
