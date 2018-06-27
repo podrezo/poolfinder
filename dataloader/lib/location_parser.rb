3fa289bcff5637cf8518296f5167b16100ac72f2
Petro Podrezo <petro@podrezo.com>
2018-06-27 13:09:39 -0400

    Output to CSV

--- dataloader/lib/location_parser.rb
+++ dataloader/lib/location_parser.rb
@@ -6,37 +6,26 @@ class LocationParser
   LATITUDE = 'Y_COORD'
   LONGITUDE = 'X_COORD'
   ADDRESS = 'address'
-  INTERSECTION = 'intersection'
   PHONE = 'telephone'
   CATEGORY = 'category'
-
-  VALID_POOL_TYPES = ['Indoor Pool', 'Outdoor Pool']
+  ID = 'locationID'
 
   def self.parse(url)
     Nokogiri::XML(open(url))
       .xpath('/POOLS/ROW')
-      .select { |row| valid_location?(row) }
       .map { |row| parse_row(row) }
       .uniq # Ignore indoor/outdoor for same location, only need it once
   end
 
   private
 
-  def self.valid_location?(row)
-    # Ignore wading pools, splash pads, etc. Only real pools
-    category = parse_column(row, CATEGORY)
-    VALID_POOL_TYPES.include?(category)
-  end
-
   def self.parse_row(row)
     {
+      id: parse_column(row, ID),
       name: parse_column(row, LOCATIONNAME),
-      coordinates: {
-        latitude: parse_column(row, LATITUDE).to_f,
-        longitude: parse_column(row, LONGITUDE).to_f,
-      },
+      latitude: parse_column(row, LATITUDE).to_f,
+      longitude: parse_column(row, LONGITUDE).to_f,
       address: parse_column(row, ADDRESS),
-      intersection: parse_column(row, INTERSECTION),
       phone: parse_column(row, PHONE),
       category: parse_column(row, CATEGORY),
     }
--- dataloader/lib/run.rb
+++ dataloader/lib/run.rb
@@ -1,15 +1,23 @@
-require_relative 'firebase'
+require 'csv'
+# require_relative 'firebase'
 require_relative 'location_parser'
 require_relative 'schedule_parser'
 
-firestore = PoolfinderFirestore.new
+# firestore = PoolfinderFirestore.new
 
-LocationParser.parse('dataloader/static_data/pools.xml').each do |pool|
-  puts "Processing location '#{pool[:name]}'"
-  firestore.create_or_update_location(pool)
+location_columns = [
+  :id, :name, :category, :address, :phone, :latitude, :longitude
+]
+csv_string = CSV.open('../tmp/pools.csv', 'w') do |csv|
+  csv << location_columns.map { |c| c.to_s }
+  LocationParser.parse('../static_data/pools.xml').each do |pool|
+    # puts "Processing location '#{:name]}'"
+    # firestore.create_or_update_location(pool)
+    csv << location_columns.map { |c| pool[c] }
+  end
 end
 
-ScheduleParser.parse_swim_times('dataloader/static_data/leisure-drop-in.html').each_with_index do |swim_time, i|
-  puts "Processing swim time #{i + 1} of #{swim_times.size}"
-  firestore.create_or_update_schedule(swim_time)
-end
+# ScheduleParser.parse_swim_times('dataloader/static_data/leisure-drop-in.html').each_with_index do |swim_time, i|
+#   puts "Processing swim time #{i + 1} of #{swim_times.size}"
+#   firestore.create_or_update_schedule(swim_time)
+# end
