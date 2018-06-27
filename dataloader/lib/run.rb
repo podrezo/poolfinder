dc7567dcc4553419e2747b6a1d6de88332c833c8
Petro Podrezo <petro@podrezo.com>
2018-06-27 13:16:35 -0400

    Schedule to csv WIP

--- dataloader/lib/run.rb
+++ dataloader/lib/run.rb
@@ -8,8 +8,8 @@ require_relative 'schedule_parser'
 location_columns = [
   :id, :name, :category, :address, :phone, :latitude, :longitude
 ]
-csv_string = CSV.open('../tmp/pools.csv', 'w') do |csv|
-  csv << location_columns.map { |c| c.to_s }
+CSV.open('../tmp/pools.csv', 'w') do |csv|
+  csv << location_columns.map(&:to_s)
   LocationParser.parse('../static_data/pools.xml').each do |pool|
     # puts "Processing location '#{:name]}'"
     # firestore.create_or_update_location(pool)
@@ -17,7 +17,14 @@ csv_string = CSV.open('../tmp/pools.csv', 'w') do |csv|
   end
 end
 
-# ScheduleParser.parse_swim_times('dataloader/static_data/leisure-drop-in.html').each_with_index do |swim_time, i|
-#   puts "Processing swim time #{i + 1} of #{swim_times.size}"
-#   firestore.create_or_update_schedule(swim_time)
-# end
+schedule_columns = [
+  :from, :to, :location_name, :activity_name
+]
+CSV.open('../tmp/schedule.csv', 'w') do |csv|
+  csv << schedule_columns.map(&:to_s)
+  ScheduleParser.parse_swim_times('../static_data/pools.xml').each do |swim_time, i|
+    # puts "Processing swim time #{i + 1} of #{swim_times.size}"
+    # firestore.create_or_update_schedule(swim_time)
+    csv << schedule_columns.map { |c| swim_time[c].to_s }
+  end
+end
