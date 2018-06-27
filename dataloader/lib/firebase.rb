29ae53ea5e3f0adb5f15c9ab1ff972c9fef803b2
Jason CheongKeeYou <jasonc@nulogy.com>
2018-06-26 22:30:29 -0400

    Refactor: replace named args with hash

--- dataloader/lib/firebase.rb
+++ dataloader/lib/firebase.rb
@@ -1,50 +1,26 @@
 require 'google/cloud/firestore'
 require 'digest'
 
-HASHER = Digest::SHA256
-
 class PoolfinderFirestore
-  
+  HASHER = Digest::SHA256
+
   def initialize
-    @_firestore = Google::Cloud::Firestore.new
-    @_locations_collection = @_firestore.col('locations')
-    @_schedule_collection = @_firestore.col('schedule')
+    @firestore = Google::Cloud::Firestore.new
+    @locations_collection = @firestore.col('locations')
+    @schedule_collection = @firestore.col('schedule')
   end
 
-  def create_or_update_location(
-    name: '',
-    latitude: 0,
-    longitude: 0,
-    address: '',
-    phone: '',
-    category: ''
-  )
+  def create_or_update_location(args)
     # The document ID is just a hash of the name to avoid data duplication
-    @_locations_collection.doc(HASHER.hexdigest(name)).set({
-      name: name,
-      coordinates: {
-        latitude: latitude,
-        longitude: longitude
-      },
-      address: address,
-      phone: phone,
-      category: category,
-    })
+    hash = HASHER.hexdigest(args[:name])
+
+    @locations_collection.doc(hash).set(args)
   end
 
-  def create_or_update_schedule(
-    location_name: '',
-    activity_name: '',
-    time_from: nil,
-    time_to: nil
-  )
+  def create_or_update_schedule(args)
     # Use a hash of all four values
-    hash = HASHER.hexdigest("#{location_name}:#{activity_name}:#{time_from}:#{time_to}")
-    @_schedule_collection.doc(hash).set({
-      activity_name: activity_name,
-      location_name: location_name,
-      from: time_from,
-      to: time_to,
-    })
+    hash = HASHER.hexdigest("#{args[:location_name]}:#{args[:activity_name]}:#{args[:from]}:#{args[:to]}")
+
+    @schedule_collection.doc(hash).set(args)
   end
 end
--- dataloader/lib/run.rb
+++ dataloader/lib/run.rb
@@ -6,38 +6,23 @@ firestore = PoolfinderFirestore.new
 
 LocationParser.parse.each do |pool|
   puts "Processing location '#{pool[:name]}'"
-  firestore.create_or_update_location(
-    name: pool[:name],
-    latitude: pool[:coordinates][:latitude],
-    longitude: pool[:coordinates][:longitude],
-    address: pool[:address],
-    phone: pool[:phone],
-    category: pool[:category],
-  )
+  firestore.create_or_update_location(pool)
 end
 
-swim_times = []
-ScheduleParser.parse.each do |location|
-  location_name = location[:location_name]
-  location[:activities].each do |activity|
-    activity_name = activity[:name]
-    activity[:hours].each do |h|
-      swim_times.push({
-        from: h[:from],
-        to: h[:to],
-        activity_name: activity_name,
-        location_name: location_name,
-      })
+swim_times = ScheduleParser.parse.flat_map do |location|
+  location[:activities].flat_map do |activity|
+    activity[:hours].map do |hours|
+      {
+        from: hours[:from],
+        to: hours[:to],
+        activity_name: activity[:name],
+        location_name: location[:location_name],
+      }
     end
   end
 end
 
-swim_times.each_with_index do |st, i|
-  puts "Processing swim time #{i+1} of #{swim_times.length}"  
-  firestore.create_or_update_schedule(
-    activity_name: st[:activity_name],
-    location_name: st[:location_name],
-    time_from: st[:from],
-    time_to: st[:to],
-  )
+swim_times.each_with_index do |swim_time, i|
+  puts "Processing swim time #{i + 1} of #{swim_times.size}"
+  firestore.create_or_update_schedule(swim_time)
 end
