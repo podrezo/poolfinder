6c3c14647de9de320e453d1b65cd769d5c6379e6
Petro Podrezo <petro@podrezo.com>
2018-06-27 23:50:01 -0400

    Save all data for each location inside the location document

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export FIRESTORE_PROJECT="poolfinder-2679b"
 export FIRESTORE_KEYFILE="$(pwd)/key.json"
 
-pushd ./dataloader/lib
+pushd ./dataloader/lib &> /dev/null
 ruby run.rb
-popd
+popd &> /dev/null
