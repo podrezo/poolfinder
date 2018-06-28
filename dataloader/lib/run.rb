2901e8095776cd893998d8a300cee775ed1ba81a
Petro Podrezo <petro@podrezo.com>
2018-06-27 23:04:52 -0400

    Scrape directly from URLS

--- dataloader/lib/run.rb
+++ dataloader/lib/run.rb
@@ -5,7 +5,7 @@ require_relative 'location_parser'
 require_relative 'schedule_parser'
 
 logger = Logger.new(STDOUT)
-logger.level = Logger::WARN
+logger.level = Logger::INFO
 
 firestore = PoolfinderFirestore.new
 
@@ -37,13 +37,16 @@ schedule_columns = [
 non_existant_locations = []
 CSV.open('../static_data/schedule.csv', 'w') do |csv|
   csv << schedule_columns.map(&:to_s)
-  ScheduleParser.parse_swim_times('../static_data/leisure-drop-in.html').each_with_index do |swim_time, i|
-    # puts "Processing swim time #{i + 1} of #{swim_times.size}"
-    # firestore.create_or_update_schedule(swim_time)
-    if !all_location_ids.include?(swim_time[:location_id]) && !non_existant_locations.include?(swim_time[:location_id])
-      logger.warn("Location with ID [#{swim_time[:location_id]}] not in locations list")
-      non_existant_locations.push(swim_time[:location_id])
+  schedule_urls_to_scrape.each do |url|
+    logger.info("Scraping url \"#{url}\"...")
+    ScheduleParser.parse_swim_times(url).each_with_index do |swim_time, i|
+      logger.info("Processing swim time #{i + 1}") if (i % 100).zero?
+      firestore.create_or_update_schedule(swim_time)
+      if !all_location_ids.include?(swim_time[:location_id]) && !non_existant_locations.include?(swim_time[:location_id])
+        logger.warn("Location with ID [#{swim_time[:location_id]}] not in locations list")
+        non_existant_locations.push(swim_time[:location_id])
+      end
+      csv << schedule_columns.map { |c| swim_time[c].to_s }
     end
-    csv << schedule_columns.map { |c| swim_time[c].to_s }
   end
 end
