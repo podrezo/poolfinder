f4aaf70459995d0a550b44daa13fb5f9df7e545f
Petro Podrezo <petro@podrezo.com>
2019-07-19 23:04:24 -0400

    Allow filtering

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,12 @@
 class HomepageController < ApplicationController
   def index
-    @locations = Location.all
+    if(params[:lat].present? and params[:lng].present?)
+      @lat = params[:lat]&.to_f
+      @lng = params[:lng]&.to_f
+      @radius = params[:radius]&.to_i || 3000
+      @locations = Location.within(@lat, @lng, @radius)
+    else
+      @locations = Location.all
+    end    
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ApplicationRecord
+  has_many :schedule, dependent: :destroy
+  scope :within, -> (latitude, longitude, distance_in_meters = 10000) {
+    where(%{
+     ST_Distance(coordinates, 'POINT(%f %f)') < %d
+    } % [longitude, latitude, distance_in_meters])
+  }
 end
